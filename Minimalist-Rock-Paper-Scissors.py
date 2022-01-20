f3f0207e4e1802718cf60e0dc8752eb27b986ad8
Luke Irwin <89871736+lukeirwin03@users.noreply.github.com>
2022-01-20 15:25:59 -0600

    Add files via upload

---
+++ Minimalist-Rock-Paper-Scissors.py
@@ -0,0 +1,22 @@
+# Classic Rock-Paper-Scissors but in as few lines as I can do. #
+import random
+choices = ["rock", "paper", "scissors"]
+player = computer = None
+repeat = True
+while repeat == True:
+   win = False
+   while win == False:
+      computer = random.choice(choices)
+      player = (input("[Rock, Paper, or Scissors]: ").lower())
+      print("Computer: " + computer + "\nPlayer: " + player)
+      if(player in choices):
+         if player == computer:
+            print(("Tie Game!").upper())
+            win = True
+         else:
+            if (player == "rock" and computer == "scissors") or (player == "paper" and computer == "rock") or (player == "scissors" and computer == "paper"):
+               print((player + " beats " + computer + "!\nPlayer Wins!").upper())
+            else:
+               print((computer + " beats " + player + "!\nComputer Wins!").upper())
+            win = True
+   repeat = ((input("Play Again? [y,n] ").lower()) == "y")
